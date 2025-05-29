56223ab53f288f5747e2e9d64409b2a533ffda68
Samat4E <122564368+Samat4E@users.noreply.github.com>
2025-05-28 23:40:30 -0700

    Complete Lab 8 with PWA setup

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Lab8-Starter
+
+## Lab Partner
+Samson
+
+## Deployed GitHub Pages URL  
+[http://127.0.0.1:5500/index.html](http://127.0.0.1:5500/index.html)
+
+## Graceful Degradation and Service Workers
+
+Graceful degradation means building a web app that still works even when some features fail or are not supported. In this project, if the user loses internet, the app still works because recipes are cached using the service worker and stored in localStorage. This keeps the app functional even when offline.
--- sw.js
+++ sw.js
@@ -9,7 +9,25 @@ self.addEventListener('install', function (event) {
     caches.open(CACHE_NAME).then(function (cache) {
       // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
       //            added to the cache when the ServiceWorker is installed
-      return cache.addAll([]);
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/assets/scripts/main.js',
+        '/assets/scripts/RecipeCard.js',
+        '/assets/styles/main.css',
+        '/assets/images/icons/0-star.svg',
+        '/assets/images/icons/1-star.svg',
+        '/assets/images/icons/2-star.svg',
+        '/assets/images/icons/3-star.svg',
+        '/assets/images/icons/4-star.svg',
+        '/assets/images/icons/5-star.svg',
+        '/recipes/1_50-thanksgiving-side-dishes.json',
+        '/recipes/2_roasting-turkey-breast-with-stuffing.json',
+        '/recipes/3_moms-cornbread-stuffing.json',
+        '/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+        '/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+        '/recipes/6_one-pot-thanksgiving-dinner.json'
+      ]);
     })
   );
 });
@@ -37,4 +55,17 @@ self.addEventListener('fetch', function (event) {
   // B8. TODO - If the request is in the cache, return with the cached version.
   //            Otherwise fetch the resource, add it to the cache, and return
   //            network response.
+  event.respondWith(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.match(event.request).then(function (cachedResponse) {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request).then(function (networkResponse) {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    })
+  );
 });
